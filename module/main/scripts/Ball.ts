1b431a0b5ec98f3042f35256d78b702aceebe82b
arthurwc <arthurwc@qq.com>
2024-05-14 17:00:12 +0800

    feat: 台球的物理运动

--- module/main/scripts/Ball.ts
+++ module/main/scripts/Ball.ts
@@ -1,15 +1,108 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Vec3 } from 'cc';
+import { yy } from '../../../../../../yy';
+import { forceRoll, rollingFull, sliding, surfaceVelocityFull } from '../../../scripts/physics/physics';
+import { passesThroughZero } from '../../../scripts/utils';
 const { ccclass, property } = _decorator;
+
+
+export enum State {
+    Stationary = "Stationary",
+    Rolling = "Rolling",
+    Sliding = "Sliding",
+    Falling = "Falling",
+    InPocket = "InPocket",
+  }
 
 @ccclass('Ball')
 export class Ball extends Component {
-    start() {
+    get pos(): Vec3 { return this.node.position; }
+    readonly vel: Vec3 = Vec3.ZERO.clone();
+    readonly rvel: Vec3 = Vec3.ZERO.clone();
+    readonly futurePos: Vec3 = Vec3.ZERO.clone();
+    state: State = State.Stationary
 
+    private static id = 0;
+    readonly id = Ball.id++;
+    static readonly transition = 0.05;
+
+
+    protected onLoad(): void {
     }
 
-    update(deltaTime: number) {
-        
+
+    protected update(dt: number): void {
+        this.updatePosition(dt);
+        if (this.state == State.Falling) {
+            // this.pocket.updateFall(this, t)
+          } else {
+            this.updateVelocity(dt)
+          }
     }
+
+    private updatePosition(t: number) {
+        this.pos.addScaledVector(this.vel, t)
+    }
+
+    private updateVelocity(t: number) {
+        if (this.inMotion()) {
+          if (this.isRolling()) {
+            this.state = State.Rolling
+            forceRoll(this.vel, this.rvel)
+            this.addDelta(t, rollingFull(this.rvel))
+            if (this.id === 1) {
+              // console.warn("update ball:", t, this.pos);
+            }
+    
+          } else {
+            this.state = State.Sliding
+            this.addDelta(t, sliding(this.vel, this.rvel))
+          }
+        }
+    }
+
+    private addDelta(t, delta) {
+        delta.v.multiplyScalar(t)
+        delta.w.multiplyScalar(t)
+        if (!this.passesZero(delta)) {
+          this.vel.add(delta.v)
+          this.rvel.add(delta.w)
+        }
+    }
+
+    private passesZero(delta) {
+        const vz = passesThroughZero(this.vel, delta.v)
+        const wz = passesThroughZero(this.rvel, delta.w)
+        const halts = this.state === State.Rolling ? vz || wz : vz && wz
+        if (halts && Math.abs(this.rvel.z) < 0.01) {
+          this.setStationary()
+          return true
+        }
+        return false
+    }
+    setStationary() {
+        this.vel.copy(Vec3.ZERO)
+        this.rvel.copy(Vec3.ZERO)
+        this.state = State.Stationary
+      }
+
+    inMotion() {
+        return (
+          this.state === State.Rolling ||
+          this.state === State.Sliding ||
+          this.isFalling()
+        )
+    }
+    isFalling() {
+        return this.state === State.Falling
+    }
+
+    isRolling() {
+        return (
+          this.vel.lengthSqr() !== 0 &&
+          this.rvel.lengthSqr() !== 0 &&
+          surfaceVelocityFull(this.vel, this.rvel).length() < Ball.transition
+        )
+      }
 }
 
 
---
+++ scripts/utils.ts
@@ -0,0 +1,49 @@
+import { Vec3 } from "cc"
+
+export const zero = new Vec3(0, 0, 0)
+export const up = new Vec3(0, 0, 1)
+
+export function vec(v) {
+  return new Vec3(v.x, v.y, v.z)
+}
+
+const upCrossVec = new Vec3()
+export function upCross(v) {
+  return upCrossVec.copy(up).cross(v)
+}
+const normVec = new Vec3()
+export function norm(v) {
+  return normVec.copy(v).normalize()
+}
+
+const vc = new Vec3()
+export function passesThroughZero(v, dv) {
+  return vc.copy(v).add(dv).dot(v) <= 0
+}
+
+export function unitAtAngle(theta) {
+  return new Vec3(1, 0, 0).applyAxisAngle(up, theta)
+}
+
+export function round(num) {
+  const sign = Math.sign(num)
+  return (sign * Math.floor((Math.abs(num) + Number.EPSILON) * 10000)) / 10000
+}
+
+export function round2(num) {
+  return Math.round((num + Number.EPSILON) * 100) / 100
+}
+
+export function roundVec(v) {
+  v.x = round(v.x)
+  v.y = round(v.y)
+  v.z = round(v.z)
+  return v
+}
+
+export function roundVec2(v) {
+  v.x = round2(v.x)
+  v.y = round2(v.y)
+  v.z = round2(v.z)
+  return v
+}
