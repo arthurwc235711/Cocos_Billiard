2fb8183c36103bcf990292dc31b5ecab06804cc2
arthurwc <arthurwc@qq.com>
2024-05-25 11:43:42 +0800

    opt： 菜单栏提交

---
+++ module/billiard_menu/scripts/BilliardMenu.ts
@@ -0,0 +1,55 @@
+import { _decorator, Component, Node } from 'cc';
+import { BaseCommonScript } from '../../../../../../main/base/BaseCommonScript';
+import { yy } from '../../../../../../yy';
+const { ccclass, property } = _decorator;
+
+@ccclass('BilliardMenu')
+export class BilliardMenu extends BaseCommonScript {
+    @property(Node)
+    nodeMore: Node = null;
+
+
+    private bMusicOn: boolean = true;
+
+    onClickMore() {
+        this.nodeMore.active = !this.nodeMore.active;
+    }
+
+    onClickMask() {
+        this.nodeMore.active = false;
+    }
+
+    onClickQuit() {
+        yy.audio.stopMusic()
+        yy.audio.stopSound()
+        yy.scene.change_bundle_scene('app_lobby', 'lobby_scene', () => {
+            let gameBundleName = this.sGameBundleName;
+            if (typeof gameBundleName === 'string' && gameBundleName.length > 0) {
+                yy.loader.releaseBundle(gameBundleName);
+            }
+            yy.loader.releaseBundle('app_casual_common');
+        });
+        yy.event.emit(yy.Event_Name.CasualCommonQuit)
+    }
+
+    onClickSound() {
+        this.bMusicOn = !this.bMusicOn;
+        yy.audio.setMusicSwitch(this.bIsMusicOn);
+        yy.audio.setSoundSwitch(this.bIsMusicOn)
+        // this.updateSoundStatus();
+        this.onClickMask();
+        yy.event.emit(yy.Event_Name.CasualCommonSound);
+    }
+
+    onClickRule() {
+        this.onClickMask();
+        yy.event.emit(yy.Event_Name.CasualCommonRule)
+    }
+
+    onClickHistory() {
+        this.onClickMask();
+        yy.event.emit(yy.Event_Name.CasualCommonHistory)
+    }
+}
+
+
