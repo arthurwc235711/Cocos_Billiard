8157db79e27db7090398686f9827a9cf7b43e204
arthurwc <arthurwc@qq.com>
2024-06-03 19:34:48 +0800

    opt: 进球后，进入轨道的滚动效果

---
+++ scripts/physics/track.ts
@@ -0,0 +1,20 @@
+import { Vec3 } from "cc";
+import { Ball } from "../../module/billiard_table/scripts/Ball";
+import { R } from "./constants";
+
+export class track {
+    readonly startPos = new Vec3(-1.5, 0.74, -0.5);
+    readonly endPos = new Vec3(-1.74, -0.77, -0.5);
+
+
+
+//
+    updateInTrack(ball:Ball, t: number) {
+        ball.vel.addScaledVector(Vec3.RIGHT, -R * t);
+        const x = ball.pos.x;
+
+    }
+
+}
+
+
