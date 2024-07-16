561877f60f3277f6c140ee3c653fdd79b4ecb6f5
arthurwc <arthurwc@qq.com>
2024-07-16 12:38:34 +0800

    opt： gameKey 修改

--- module/billiard_match/scripts/BilliardMatchView.ts
+++ module/billiard_match/scripts/BilliardMatchView.ts
@@ -76,7 +76,7 @@ export class BilliardMatchView extends BaseCommonScript {
 
     onClickGoBack() {
         BilliardService.instance.sendLeaveMatching();
-        yy.user.setLobbyOpenGameLevel({ gameKey: "billiard" });
+        yy.user.setLobbyOpenGameLevel({ gameKey: "billiard8ball" });
         if (yy.Event_Name.CasualCommonQuit) { // 桌球游戏内调用
             yy.event.emit(yy.Event_Name.CasualCommonQuit);
         }
