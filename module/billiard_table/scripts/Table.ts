eaea18a7239beec06569e87d6977f74cf11e03c1
arthurwc <arthurwc@qq.com>
2024-08-12 16:02:28 +0800

    opt： Cushion 裤边碰撞 参数调整  & 碰撞函数调整

--- module/billiard_table/scripts/Table.ts
+++ module/billiard_table/scripts/Table.ts
@@ -4,7 +4,7 @@ import { Collision } from '../../../scripts/physics/collision';
 import { TableGeometry } from './TableGeometry';
 import { yy } from '../../../../../../yy';
 import { Cushion } from './Cushion';
-import { bounceHanBlend, cueToSpin } from '../../../scripts/physics/physics';
+import { bounceHan, bounceHanBlend, cueToSpin } from '../../../scripts/physics/physics';
 import { BilliardData } from '../../../data/BilliardData';
 import { R } from '../../../scripts/physics/constants';
 import { Outcome } from './Outcome';
@@ -36,7 +36,7 @@ export class Table extends BaseCommonScript {
     balls:Ball[];
     pairs: Pair[]; // 球对
     outcome: Outcome[] = [];
-    cushionModel = bounceHanBlend
+    cushionModel = bounceHan
     cueBall:Ball = null;
 
     shotBall: Ball = null;
