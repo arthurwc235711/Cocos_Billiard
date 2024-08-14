1c4496732e371022b2ca7d822a6133c284c8f2eb
arthurwc <arthurwc@qq.com>
2024-08-14 14:07:06 +0800

    fix：BUG--匹配界面配到人扣余额的时候右上角显示扣了双倍，实际结算是正确的。

--- module/billiard_match/scripts/BilliardMatchView.ts
+++ module/billiard_match/scripts/BilliardMatchView.ts
@@ -208,7 +208,7 @@ export class BilliardMatchView extends BaseCommonScript {
         this.setPlayerInfo(this.otherUI, otherInfo[0], otherInfo[0].moneyBet);
         this.rollNum(this.nodeAddGold.getChildByName("Label").getComponent(Label), 0, msg.chipPot, 2.5);
 
-        this.lableMyGold.string = yy.money.formatMoney(yy.user.getTotalMoney() - msg.chipPot, false);
+        this.lableMyGold.string = yy.money.formatMoney(yy.user.getTotalMoney() - myInfo[0].moneyBet, false);
     }
 
     rollNum(label:Label, orgNum:number, distNum: number, totalTimes: number) {
